e5b66e254512850d7baea8ff0d5d3d8a9fd40d74
taru-boy <tarutaru.com.5021@gmail.com>
2024-11-11 21:55:45 +0900

    day17_setオブジェクト,urlparseライブラリ

--- 03_internal_links/internallink_copy.py
+++ 03_internal_links/internallink_copy.py
@@ -1,33 +1,41 @@
 # プログラム終了のためのsysのimport
 import sys
+
 # 正規表現を使うためのreをimport
 import re
+
 # HTTPリクエストを送るためのrequestsをimport
 import requests
+
 # BeautifulSoupをimport
 from bs4 import BeautifulSoup
+
 # URLからドメインを取得する為にurlparseをimport
 from urllib.parse import urlparse
+
 # ネットワーク図を作成する為にnetworkxをimport
 import networkx as nx
+
 # ネットワーク図を描画する為にmatplotlibをimport
 import matplotlib.pyplot as plt
 
+
+def main():
     """
     メインの実行部分:調べたいURLはanl_urlに入力する
     """
 
-    # 空のセットを用意
+    pages = set()  # 空のセットを用意
+    anl_url = "https://hashikake.com/RegEx"  # 内部リンクを調べたいURL
 
-    # 内部リンクを調べたいURL
+    parsed_url = urlparse(anl_url)
+    scheme = parsed_url.scheme
+    base_domain = parsed_url.netloc
 
-    # https://またはhttp://からはじまる基準のホームページのURL
+    # scheme（https）と netloc（hashikake.com）を結合してベースURLを作成
+    base_url = f"{scheme}://{base_domain}/"
 
-    # match_urlはマッチオブジェクトなので、そこからURLだけを取り出す
-
-    # 正規表現で使うためにドメイン名の取得
-
-    #　内部リンクの取得
+    # 　内部リンクの取得
 
     # 内部リンクが存在するなら
 
@@ -43,14 +51,12 @@ import matplotlib.pyplot as plt
 
     # 内部リンクが存在しない場合はプログラムの終了
 
-
-
     """
     /で始まるものと、base_urlから始まるもの//ドメインから始まるもの
     全ての内部リンクを取得して、重複を除去してpagesに収集する＋
     内部リンク数を出力
     """
-        
+
     # 正規表現の中で変数を使う時はf文字列またはformatを使う
     # /で始まって//を含まないURLと、https://ドメインから始まるもの、//ドメインから始まるもの
 
@@ -68,7 +74,6 @@ import matplotlib.pyplot as plt
 
     # セットの中に内部リンクとして追加
 
-
     """
     調査URLを中心としたネットワーク図の作成
     """
@@ -77,7 +82,7 @@ import matplotlib.pyplot as plt
 
     # indexの0に文字列"start_url"を追加
 
-    #　空のグラフの作成　有向グラフ
+    # 　空のグラフの作成　有向グラフ
 
     # リストの最初の要素を中心として放射状に頂点と辺の追加
 
@@ -98,7 +103,7 @@ import matplotlib.pyplot as plt
     """
     URLのhttp://を省略してネットワーク図を見やすくするための調整
     privacyページやcontactページなどの無駄な内部リンクページの除去
-    """    
+    """
 
     # 内部リンクのURLから効果の薄い内部リンクをre.sub()で消していく base_url https://hashikake.com //hashikake.com
 
@@ -107,3 +112,7 @@ import matplotlib.pyplot as plt
     # short_linksをセットに変更(重複の削除)
 
     # ""を削除　# discardだとキーがなくてもエラーにはならない。removeだとエラーになる
+
+
+if __name__ == "__main__":
+    main()
